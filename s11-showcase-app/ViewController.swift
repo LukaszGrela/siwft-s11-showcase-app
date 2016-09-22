73f83c4423a3859578174275a72dad97f47ca9dd
Lukasz <lukasz.grela@gmail.com>
2016-09-22 10:49:56 +0100

    Initial Commit

---
+++ s11-showcase-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  s11-showcase-app
+//
+//  Created by Lukasz Grela on 22.09.2016.
+//  Copyright © 2016 Commelius Solutions Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
