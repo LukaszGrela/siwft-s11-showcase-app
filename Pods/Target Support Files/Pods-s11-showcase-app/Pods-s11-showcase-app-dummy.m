ae96fe152f610c2aac8474b41a5a7246f34d5108
Lukasz <lukasz.grela@gmail.com>
2016-09-28 20:45:02 +0100

    pods

---
+++ Pods/Target Support Files/Pods-s11-showcase-app/Pods-s11-showcase-app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_s11_showcase_app : NSObject
+@end
+@implementation PodsDummy_Pods_s11_showcase_app
+@end
---
+++ Pods/Target Support Files/Pods-s11-showcase-app/Pods-s11-showcase-app-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_s11_showcase_appVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_s11_showcase_appVersionString[];
+
